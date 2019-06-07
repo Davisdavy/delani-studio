03f701ce26bc1ef9babf729acc1007993bb66ebf
Davisdavy <davisdvay96@gmail.com>
2019-06-07 19:34:20 +0300

    Add overlay on portfolio images

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    $(".overlay").mouseover(function(){
+        $(this).css({
+            "opacity":"5",
+            "transition": "2s",
+            "cursor": "pointer"
+        });
+        
+});
+    
+});
