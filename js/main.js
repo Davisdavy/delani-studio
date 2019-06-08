fcae92fceb4938cd8c0434e757ee0a4316eaffc3
Davisdavy <davisdvay96@gmail.com>
2019-06-08 10:52:58 +0300

    Hide Services image

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 
 $(document).ready(function () {
+    $(".hidden").click(function (){
+        $(".descript-hide").toggle();
+        $(".hidden").toggle();
+    });
+
+    $(".hidden2").click(function (){
+        $(".descript-hide2").toggle();
+        $(".hidden2").toggle();
+        
+    });
+
+    $(".hidden3").click(function (){
+        $(".descript-hide3").toggle();
+        $(".hidden3").toggle();
+    });
+
+
+
     $("div.overlay").hover(function(){
         $(this).css({
-            "opacity":"1",
+            "opacity":"5",
             "transition": "2s",
             "cursor": "pointer"
         });
